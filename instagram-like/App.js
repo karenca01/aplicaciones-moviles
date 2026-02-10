bb4c47b70a32888054d7b53d8e3ff7413029b9bb
karenca01 <kcervantes01@alumnos.uaq.mx>
2026-02-09 21:33:34 -0600

    dam32-325792-p6

--- instagram-like/App.js
+++ instagram-like/App.js
@@ -1,10 +1,40 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
 import { Heart, MessageCircle, Send, Bookmark } from 'lucide-react-native';
+import { useState } from 'react';
 
+const App = () => {
 
-export default function App() {
+  const [numberLikes, setNumberLikes] = useState(0);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const [isSaved, setIsSaved] = useState(false);
+  // const [isSaved, setIsSaved] = useState('gray');
+  // const [numberSaves, setNumberSaves] = useState(0);
+
+  // const handleLikesAndColor = () => {
+  //   setNumberLikes(numberLikes + 1);
+  //   setLikeColor('#e54848')
+  // };
+
+  const handleLikesAndColor = () => {
+    if (isLiked) {
+      setNumberLikes(numberLikes - 1);
+    } else {
+      setNumberLikes(numberLikes + 1);
+    }
+    setIsLiked(!isLiked);
+  };
+
+  // const saveColor = () => {
+  //   setSavesColor('#e9f26eff')
+  // };
+
+  const handleSaves = () => {
+    setIsSaved(!isSaved);
+  };
+
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <View style={styles.header}>
         <Image
           source={require('./assets/images/instagram-logo.png')}
@@ -12,8 +42,10 @@ export default function App() {
           resizeMode="contain"
         />
       </View>
+
       <View style={styles.contentContainer}>
         <View style={styles.postContainer}>
+
           <View style={styles.userContainer}>
             <View style={styles.avatar} />
             <Text style={styles.usernameText}>username</Text>
@@ -29,12 +61,38 @@ export default function App() {
 
           <View style={styles.interactionsContainer}>
             <View style={styles.iconsLeft}>
-              <Heart color="black" size={24} />
+
+              {/* <TouchableOpacity onPress={handleLikesAndColor}>
+                <Heart size={24} color={'black'} fill={likeColor} />
+              </TouchableOpacity> */}
+
+              <TouchableOpacity onPress={handleLikesAndColor}>
+                <Heart
+                  size={24}
+                  color={'black'}
+                  fill={isLiked ? '#cc0606ff' : 'gray'}
+                />
+              </TouchableOpacity>
+
+              <Text style={styles.numberLikesC}>
+                {numberLikes} likes
+              </Text>
+
               <MessageCircle color="black" size={24} />
               <Send color="black" size={24} />
             </View>
 
-            <Bookmark color="black" size={24} />
+            {/* <TouchableOpacity onPress={saveColor}>
+              <Bookmark color="black" fill={savesColor} size={24} />
+            </TouchableOpacity> */}
+
+            <TouchableOpacity onPress={handleSaves}>
+              <Bookmark
+                color="black"
+                fill={isSaved ? '#e9f26eff' : 'gray'}
+                size={24}
+              />
+            </TouchableOpacity>
           </View>
 
           <View style={styles.descriptionContainer}>
@@ -43,17 +101,20 @@ export default function App() {
               Listen, listen!
             </Text>
           </View>
+
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    // alignItems: 'center',
   },
   header: {
     width: '100%',
@@ -124,4 +185,9 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     gap: 15,
   },
+  numberLikesC: {
+    paddingHorizontal: 5,
+    paddingVertical: 5,
+    fontWeight: 'bold',
+  }
 });
